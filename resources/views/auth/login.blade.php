085dd710c4b3bf966974fcfe026d3782469fe052
David Guadalupe Vargas Lopez <david.vargas7480@alumnos.udg.mx>
2026-09-02 12:57:43 -0600

    Quitar correo de ejemplo del campo de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Correo electrónico')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="admin@cucei.com" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="correo@ejemplo.com" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
